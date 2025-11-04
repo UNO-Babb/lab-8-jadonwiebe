6620e683dc5d06e43fbf6d1a4dec86b4e6795851
jadonwiebe <jadonwiebe@gmail.com>
2025-11-04 22:37:21 +0000

    All done

--- ProcessData.py
+++ ProcessData.py
@@ -1,15 +1,38 @@
 #ProcessData.py
-#Name:
-#Date:
-#Assignment:
+#Name:Jadon Wiebe
+#Date:11/04/2025
+#Assignment:Lab 8
 
 import random
 
 def main():
 
   #Open the files we will be using
+  Data = []
   inFile = open("names.dat", 'r')
   outFile = open("StudentList.csv", 'w')
+  for line in inFile:
+    info = line.split(" ")
+    lastName = info[1]
+    check = len(lastName)
+    if check <5:
+      need = 5-check
+      for i in range(need):
+        add = "X"
+        last = lastName + add
+    firstName = info[0]
+    IDnum = info[3]
+    user = firstName[0] + lastName + IDnum[8:]
+    grade = info[5]
+    major = info[6]
+    grade_major = major[0:3] + " - " + grade[0:2]
+
+    Data.append((lastName, firstName, user, grade_major))
+  for line in Data:
+    output = line[0] + ", " + line[1] + ", " + line[2] + ", " + line[3]
+    #check over last line
+    outFile.write(output)
+
 
   #Process each line of the input file and output to the CSV file
 
